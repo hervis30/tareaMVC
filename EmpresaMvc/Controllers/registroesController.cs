c61da12fa88b9599d0ba9f4d21e963dcfa1437eb
Hervis <hervisjonat@gmail.com>
2021-11-18 17:28:33 -0500

    trareamvc

---
+++ EmpresaMvc/Controllers/registroesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmpresaMvc.Models;
+
+namespace EmpresaMvc.Controllers
+{
+    public class registroesController : Controller
+    {
+        private empresaEntities db = new empresaEntities();
+
+        // GET: registroes
+        public ActionResult Index()
+        {
+            return View(db.registro.ToList());
+        }
+
+        // GET: registroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registro registro = db.registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registro);
+        }
+
+        // GET: registroes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: registroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codEmpresa,nombre,sector,contacto,email,celular")] registro registro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registro.Add(registro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(registro);
+        }
+
+        // GET: registroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registro registro = db.registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registro);
+        }
+
+        // POST: registroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codEmpresa,nombre,sector,contacto,email,celular")] registro registro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registro);
+        }
+
+        // GET: registroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registro registro = db.registro.Find(id);
+            if (registro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registro);
+        }
+
+        // POST: registroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registro registro = db.registro.Find(id);
+            db.registro.Remove(registro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
